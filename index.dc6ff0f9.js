20cdd75f5ff3c692d9841efa043f78155b7c30db
OlhaMal <117120298+OlhaMal@users.noreply.github.com>
2022-12-18 14:42:14 +0000

    Deploying to gh-pages from @ OlhaMal/goit-big-boom-team-project@3583ff971532942e0e3b290141d8be2ccf7d8f9f 🚀

---
+++ index.dc6ff0f9.js
@@ -0,0 +1,2 @@
+!function(){var e={openModalBtn:document.querySelector("[data-menu-open]"),closeModalBtn:document.querySelector("[data-menu-close]"),modal:document.querySelector("[data-menu]")};function n(){e.modal.classList.toggle("is-open")}e.openModalBtn.addEventListener("click",n),e.closeModalBtn.addEventListener("click",n)}();
+//# sourceMappingURL=index.dc6ff0f9.js.map
